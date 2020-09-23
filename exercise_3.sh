67bfeb8894f63f01e4d826c0e0f7cec22d09571d
Sehazen <sehazen56@gmail.com>
2020-09-23 00:47:26 -0400

    Minimum Values Identified, Difference Implementation Needed

--- exercise_3.sh
+++ exercise_3.sh
@@ -1,2 +1,9 @@
 #Usage bash exercise_3.sh wages.csv
 #Returns difference between min wage of 16 years of education and 12 years of education
+
+
+#Minimum Values for 12 and 16 years of education
+
+cat wages.csv | cut -d "," -f 3,4 | egrep "^12" | sort -n -k 2 | head -n 1 | cut -d "," -f 2
+
+cat wages.csv | cut -d "," -f 3,4 | egrep "^16" | sort -n -k 2 | head -n 1 | cut -d  "," -f 2

0606e96dbd07123fe31aad3ca4d8a2781fd4bc18
Sehazen <sehazen56@gmail.com>
2020-09-22 23:23:43 -0400

    Edited Sorting Pipline for Male and Female

--- exercise_1.sh
+++ exercise_1.sh
@@ -1,4 +1,7 @@
 #Usage bash exercise_1.sh filename
 #Sort by unique years of experience, while keeping pairs intact
 
-cat wages.csv | cut -d , -f 1,2 | sed -E 's/,/ /g' >> split.txt
+rm uniqpair.txt
+
+cat "$1" | cut -d , -f 1,2 | sed -E 's/,/ /g' | grep "female" | sort -n -k 2 | uniq -f 1 >> uniqpair.txt
+cat "$1" | cut -d , -f 1,2 | sed -E 's/,/ /g' | grep -w "male" | sort -n -k 2 | uniq -f 1 >> uniqpair.txt

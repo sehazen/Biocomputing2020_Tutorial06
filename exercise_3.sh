f85005291929041afd639910a363c96275ea5c64
Sehazen <sehazen56@gmail.com>
2020-09-23 01:00:35 -0400

    Values Assigned to Variables, Issues with Hint for Difference

--- exercise_3.sh
+++ exercise_3.sh
@@ -2,8 +2,12 @@
 #Returns difference between min wage of 16 years of education and 12 years of education
 
 
-#Minimum Values for 12 and 16 years of education
+#Minimum Values for 12 and 16 years of education assigned into two variables
 
-cat wages.csv | cut -d "," -f 3,4 | egrep "^12" | sort -n -k 2 | head -n 1 | cut -d "," -f 2
+min_12=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^12" | sort -n -k 2 | head -n 1 | cut -d "," -f 2)
 
-cat wages.csv | cut -d "," -f 3,4 | egrep "^16" | sort -n -k 2 | head -n 1 | cut -d  "," -f 2
+min_16=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^16" | sort -n -k 2 | head -n 1 | cut -d  "," -f 2)
+
+echo "$min_12"
+echo "$min_16"
+

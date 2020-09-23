c7c50fa442a7229d72e93041095f7f1448bd2023
Sehazen <sehazen56@gmail.com>
2020-09-23 19:24:08 -0400

    Difference Calculation Issues Resolved

--- exercise_3.sh
+++ exercise_3.sh
@@ -4,10 +4,9 @@
 
 #Minimum Values for 12 and 16 years of education assigned into two variables
 
-min_12=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^12" | sort -n -k 2 | head -n 1 | cut -d "," -f 2)
+min_12=$(cat "$1" | cut -d "," -f 3,4 | egrep "^12" | sort -n -k 2 | head -n 1 | cut -d "," -f 2)
 
-min_16=$(cat wages.csv | cut -d "," -f 3,4 | egrep "^16" | sort -n -k 2 | head -n 1 | cut -d  "," -f 2)
+min_16=$(cat "$1" | cut -d "," -f 3,4 | egrep "^16" | sort -n -k 2 | head -n 1 | cut -d  "," -f 2)
 
-echo "$min_12"
-echo "$min_16"
 
+echo " $min_16 - $min_12" | tr -d "\r" | bc

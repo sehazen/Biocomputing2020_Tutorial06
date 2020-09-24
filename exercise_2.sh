8a535101f3499dfcc6ea0d6d60b7bffedda50402
Keara G <kgilliga@nd.edu>
2020-09-24 19:28:29 -0400

    Good copy

--- exercise_2.sh
+++ exercise_2.sh
@@ -1,2 +1,13 @@
-cat wages.csv | sort -k4,4 -t "," wages.csv 
+ cat wages.csv | sort -k4,4 -t "," wages.csv | cut -d , -f 1-2,4 | tail -n 11 | grep -E "female"| wc -echo "lowest earner"
+
+cat wages.csv | sort -k4,4 -t "," wages.csv | cut -d , -f 1-2,4 | head -n 1 
+
+
+echo "highest earner"
+
+cat wages.csv | sort -k4,4 -t "," wages.csv | cut -d , -f 1-2,4 | tail -n 2 | head -n 1
+
+Echo “females in the top 10 of earners”
+
+cat wages.csv | sort -k4,4 -t "," wages.csv | cut -d , -f 1-2,4 | tail -n 11 | grep - E “female” | wc -l
 

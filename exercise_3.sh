f62ded2c31fa05e2e2c24cc817f050644e80d3fe
Sehazen <sehazen56@gmail.com>
2020-09-24 18:27:11 -0400

    Added Comments

--- exercise_3.sh
+++ exercise_3.sh
@@ -9,4 +9,6 @@ min_12=$(cat "$1" | cut -d "," -f 3,4 | egrep "^12" | sort -n -k 2 | head -n 1 |
 min_16=$(cat "$1" | cut -d "," -f 3,4 | egrep "^16" | sort -n -k 2 | head -n 1 | cut -d  "," -f 2)
 
 
+#Difference Returned to Stdout
+
 echo " $min_16 - $min_12" | tr -d "\r" | bc

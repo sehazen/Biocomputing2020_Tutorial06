e25b5f6b65dfbb02880f0fed494d01cdbb581722
Sehazen <sehazen56@gmail.com>
2020-09-24 20:06:43 -0400

    Added More Descriptive Stdout Titles

--- exercise_2.sh
+++ exercise_2.sh
@@ -1,16 +1,17 @@
 #Usage bash exercise_2.sh filename
 #Identify highest and lowest earners, as well as the number of women in the top 10 earners
 
-echo "lowest earner"
+echo "The gender, years of experience, and wage of the lowest earner:"
 
 cat wages.csv | sort -k4,4 -t "," wages.csv | cut -d , -f 1-2,4 | head -n 1 
 
 
-echo "highest earner"
+echo "The gender, years of experience, and wage of the highest earner:"
 
 cat wages.csv | sort -k4,4 -t "," wages.csv | cut -d , -f 1-2,4 | tail -n 2 | head -n 1
 
-echo "females in top 10 of earners"
+
+echo "The number of females in the list of top 10 highest earners:"
 
 cat wages.csv | sort -k4,4 -t "," wages.csv | cut -d , -f 1-2,4 | tail -n 11 |grep -E "female" | wc -w
 

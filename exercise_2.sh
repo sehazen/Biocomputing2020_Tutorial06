01633caa0b3768f4a1594e1b4df54caba87e64e6
Sehazen <sehazen56@gmail.com>
2020-09-24 20:24:19 -0400

    Revised Sort Section of the Pipelines

--- exercise_2.sh
+++ exercise_2.sh
@@ -2,16 +2,8 @@
 #Identify highest and lowest earners, as well as the number of women in the top 10 earners
 
 echo "The gender, years of experience, and wage of the lowest earner:"
-
-cat "$1" | sort -k4,4 -t ","| cut -d , -f 1-2,4 | head -n 1 
-
-
+cat "$1" | sort -k 4 -t , -n | cut -d , -f 1-2,4 | head -n 1
 echo "The gender, years of experience, and wage of the highest earner:"
-
-cat "$1" | sort -k4,4 -t "," | cut -d , -f 1-2,4 | tail -n 2 | head -n 1
-
-
+cat "$1" | sort -k 4 -t , -n | cut -d , -f 1-2,4 | tail -n 2 | head -n 1
 echo "The number of females in the list of top 10 highest earners:"
-
-cat "$1" | sort -k4,4 -t "," | cut -d , -f 1-2,4 | tail -n 11 |grep -E "female" | wc -w
-
+cat "$1" | sort -k 4 -t , -n | cut -d , -f 1-2,4 | tail -n 11 |grep -E "female" | wc -w

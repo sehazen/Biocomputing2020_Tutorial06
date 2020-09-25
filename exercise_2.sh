2c7b7db760270203a970e6cbb4cb011d153d48ca
Sehazen <sehazen56@gmail.com>
2020-09-24 20:09:43 -0400

    Removed uncessary uses of file name

--- exercise_2.sh
+++ exercise_2.sh
@@ -3,15 +3,15 @@
 
 echo "The gender, years of experience, and wage of the lowest earner:"
 
-cat wages.csv | sort -k4,4 -t "," wages.csv | cut -d , -f 1-2,4 | head -n 1 
+cat "$1" | sort -k4,4 -t ","| cut -d , -f 1-2,4 | head -n 1 
 
 
 echo "The gender, years of experience, and wage of the highest earner:"
 
-cat wages.csv | sort -k4,4 -t "," wages.csv | cut -d , -f 1-2,4 | tail -n 2 | head -n 1
+cat "$1" | sort -k4,4 -t "," | cut -d , -f 1-2,4 | tail -n 2 | head -n 1
 
 
 echo "The number of females in the list of top 10 highest earners:"
 
-cat wages.csv | sort -k4,4 -t "," wages.csv | cut -d , -f 1-2,4 | tail -n 11 |grep -E "female" | wc -w
+cat "$1" | sort -k4,4 -t "," | cut -d , -f 1-2,4 | tail -n 11 |grep -E "female" | wc -w
 
